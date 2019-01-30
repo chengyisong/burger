ca2f28239b238184c70ce0d67fcd2aca5c11db35
CHENGYI SONG <carrie.song@beyondsoft.com>
2019-01-29 22:51:50 -0800

    Testing JAWSDB

--- config/connection.js
+++ config/connection.js
@@ -1,22 +1,24 @@
 var mysql = require("mysql");
 
-var connection
+var JAWSDB_URL = "mysql://m3q63vzcieh1x5qd:dhq9unihhivax8hk@axxb6a0z2kydkco3.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306/srob74ryon13jdna"
 
-console.log(process.env.JAWSDB_URL);
+var connection = mysql.createConnection(JAWSDB_URL);
 
-if (process.env.JAWSDB_URL) {
-    console.log("establish connection")
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+// console.log(process.env.JAWSDB_URL);
 
-} else {
-    connection = mysql.createConnection({
-        host: "localhost",
-        port: 3306,
-        user: "root",
-        password: "900922",
-        database: "burgers_db"
-    })
-}
+// if (process.env.JAWSDB_URL) {
+//     console.log("establish connection")
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+// } else {
+//     connection = mysql.createConnection({
+//         host: "localhost",
+//         port: 3306,
+//         user: "root",
+//         password: "900922",
+//         database: "burgers_db"
+//     })
+// }
 
 connection.connect(function (err) {
     if (err) {
